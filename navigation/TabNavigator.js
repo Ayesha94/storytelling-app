e47d64748ee2d33ad2cd2bb82a9136db872f02e9
Ayesha94 <gupta.ayesh@gmail.com>
2021-07-11 14:48:36 +0530

    Loaded temp.js and other asset files

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,12 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { StyleSheet } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as React from 'react';
+import {RFValue} from 'react-native-responsive-fontsize';
+
 import Feed from '../screens/Feed';
 import CreateStory from '../screens/CreateStory';
 
 //we are creating bottom tab navigator called Tab
-const Tab=createBottomTabNavigator();
+const Tab=createMaterialBottomTabNavigator();
 
 const BottomTabNavigator=()=> {
   return (
@@ -14,24 +16,27 @@ const BottomTabNavigator=()=> {
    
    <Tab.Navigator
       //with the help of attributes we define specifications
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
       //in screen options we define icons for all routes:Feed and CreateStory
       screenOptions={({route})=>({
         tabBarIcon:({focused, color,size})=>{
           let iconName;
           if(route.name==="Feed"){
-            iconName=focused?'book':'book-outline';
+            iconName=focused
+            ?'home'
+            :'home-outline';
           }else if(route.name==='CreateStory'){
-            iconName=focused?'create':'create-outline';
+            iconName=focused
+            ?'add-circle'
+            :'add-circle-outline';
           }
-          return <Ionicons name={iconName} size={size} color={color}/>;
+          return <Ionicons name={iconName} color={color}  size={20}/>;
 
         },
       })}
-      //another attribute
-      tabBarOptions={{
-        activeTintColor:"tomato",
-        inactiveTintColor:"gray",
-        }} 
+      activeColor={"#ee8249"}
+      inactiveColor={"gray"}
     >
      <Tab.Screen name="Feed" component={Feed}/>
      <Tab.Screen name="CreateStory" component={CreateStory}/>
@@ -40,6 +45,19 @@ const BottomTabNavigator=()=> {
    
   );
 }
-
+const styles=StyleSheet.create({
+    bottomTabStyle:{
+        backgroundColor:"#2f345d",
+        height:'8%',
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        overflow:"hidden",
+        position:"absolute"
+    },
+    icons:{
+        width:RFValue(30),
+        height:RFValue(30)
+    }
+})
 export default BottomTabNavigator
 
